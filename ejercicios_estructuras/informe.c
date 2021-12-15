f3e32aa803f7bfa170ba4272dc23d9f0d4350a05
dani <danielromero891@gmail.com>
2021-12-15 23:29:48 +0100

    segundo ejercicio terminado pero falta pulirlo (filtros etc)

---
+++ ejercicios_estructuras/informe.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+    struct registro{
+        char codigo[20]; 
+        int precio;
+        int cantidad;
+    };
+
+    FILE *fich;
+
+    if ((fich= fopen("ventas.txt","rb")) == NULL){
+        printf("Error al abrir el fichero\n");
+        return -1;
+    };
+
+    int total=0;
+    int flag=0;
+    int importe;
+    struct registro r;
+
+    fread(&r,sizeof(r), 1, fich);
+    
+    while (! feof(fich)){
+        importe=((r.precio)*(r.cantidad));
+        printf("%s - %d \n", r.codigo, importe);        
+        total=total+importe;
+        fread(&r, sizeof(r), 1, fich);
+    }
+
+    printf("\n TOTAL - %D", total);
+
+    fclose(fich);
+
+    return 0;
+}
